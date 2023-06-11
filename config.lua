38f7e9152b15b183d67372f43f6e88fd10e98d69
Pal-PE <52689062+Pal-PE@users.noreply.github.com>
2023-06-11 14:44:38 +0100

    first build

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {
+    token = "NoTai8eWf", -- Your secret server token
+    rejection = "Not whitelisted",
+    debug = false -- Leave this to false unless instructed to by support
+}
---
+++ server.lua
@@ -0,0 +1,39 @@
+-- Function to check if the player is whitelisted
+function CheckWhitelist(deferrals, identifiers, name)
+    -- Handle identifiers
+    local identifiersString = ""
+    for i = 1, #identifiers do
+        -- Steam
+        if string.find(identifiers[i], "steam:") then
+            identifiersString = identifiersString .. "&steam=" .. tonumber(string.sub(identifiers[i], string.len("steam:") + 1, string.len(identifiers[i])), 16)
+        end
+        -- FiveM
+        if string.find(identifiers[i], "license:")  then
+            identifiersString = identifiersString .. "&fivem=" .. string.sub(identifiers[i], string.len("license:") + 1, string.len(identifiers[i]))
+        end
+        -- Discord
+        if string.find(identifiers[i], "discord:")  then
+            identifiersString = identifiersString .. "&discord=" .. string.sub(identifiers[i], string.len("discord:") + 1, string.len(identifiers[i]))
+        end
+    end
+    -- Check with API if player should be allowed
+    local url = "https://authcx-api.herokuapp.com/whitelist/check/fivem/?token=" .. Config.token .. identifiersString
+    PerformHttpRequest(url, function(errorCode, resultData, resultHeaders)
+        if errorCode == 200 then
+            print()
+            deferrals.done()
+        else
+            deferrals.done(Config.rejection)
+        end
+    end, "GET", "", {})
+end
+
+-- Event triggered when a player joins the server
+AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
+    local src = source
+    local identifiers = GetPlayerIdentifiers(src) -- Assuming the first identifier is the Steam ID
+    deferrals.defer()
+    deferrals.update("Contacting AuthCx API..")
+    Citizen.Wait(1000)
+    CheckWhitelist(deferrals, identifiers, name)
+end)
